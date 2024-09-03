27e3c633b598e06ead73277b86d81276b2ed26be
Pedro Novais <phenrique101007@gmail.com>
2024-09-02 22:53:45 -0300

    Cria componentes Input e Button

---
+++ src/pages/Login/Login.tsx
@@ -0,0 +1,40 @@
+import { useState } from "react";
+import { Button } from "../../components/Button/Button";
+import { Input } from "../../components/Input/Input";
+import { Layout } from "../../components/Layout/Layout";
+
+export default function Login() {
+    const [login, setLogin] = useState('')
+    const [password, setPassword] = useState('')
+
+    const handleLogin = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setLogin(event.target.value);
+    }
+
+    const handlePassword = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setPassword(event.target.value);
+    }
+
+    const handleClick = () => {
+        console.log(login, password);
+    }
+
+    return (
+        <Layout>
+            <h1>Login</h1>
+            <form>
+                <Input type="text" name="login" id="login" onChange={handleLogin} label="Login:" />
+                <Input
+                    type="text"
+                    name="password"
+                    id="password"
+                    onChange={handlePassword}
+                    label="Senha:"
+                    placeholder="Digite sua senha"
+                />
+
+                <Button type="button" onClick={handleClick}>Login</Button>
+            </form>
+        </Layout>
+    )
+}
