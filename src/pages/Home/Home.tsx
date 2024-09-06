2d992a55116766d280d353e66bc0ef7f71996a79
pedroonovais <phenrique101007@gmail.com>
2024-09-05 21:00:23 -0300

    Adicionar requisição a api de cidades do brasil api

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,19 +1,38 @@
 import { useEffect, useState } from "react";
 import { Layout } from "../../components/Layout/Layout";
-import { cityMock } from "../../mocks/CityMock";
+//import { cityMock } from "../../mocks/CityMock";
 
 export default function Home() {
     const [city, setCity] = useState()
     
+    const LoadCities = () => {
+        // GET
+        fetch('https://brasilapi.com.br/api/cptec/v1/cidade')
+            .then( response => {
+                return response.json()
+            })
+            .then( data => {
+                console.log(data)
+                setCity(data)
+            })
+    }
+
     useEffect(() => {
-        setCity(cityMock)
-    }, [])
+        // setCity(cityMock)
+        if (!city.lenght){
+            return
+        }
+        LoadCities()
+    }, [city])
 
     return (
         <Layout>
             <h1>Home</h1>
+            { city.map((item: any) => {
+                <p key={item.id}>{item?.nome} / {item?.estado}</p>
+            })}
 
-            <p>{city?.cidade} / {city?.estado}</p>
+            
         </Layout>
     );
 }
