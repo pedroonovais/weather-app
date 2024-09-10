70c3e4b3461c91c5cb01ad803151adfc9cdd29ec
Laboratório FIAP <labsfiap@fiap.com.br>
2024-09-09 22:54:02 -0300

    Aula 09/09

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -2,37 +2,45 @@ import { useEffect, useState } from "react";
 import { Layout } from "../../components/Layout/Layout";
 //import { cityMock } from "../../mocks/CityMock";
 
-export default function Home() {
-    const [city, setCity] = useState()
-    
-    const LoadCities = () => {
+export default async function Home() {
+    const [city, setCity] = useState([])
+
+    const LoadCities = async () => {
         // GET
-        fetch('https://brasilapi.com.br/api/cptec/v1/cidade')
-            .then( response => {
-                return response.json()
-            })
-            .then( data => {
-                console.log(data)
-                setCity(data)
-            })
+        // fetch('https://brasilapi.com.br/api/cptec/v1/cidade')
+        //     .then( response => {
+        //         return response.json()
+        //     })
+        //     .then( data => {
+        //         console.log(data)
+        //         setCity(data)
+        //     })
+
+        try {
+            const response = await fetch('https://brasilapi.com.br/api/cptec/v1/cidade')
+            const data = await response.json()
+            setCity(data)
+        } catch (error) {
+            console.log(error)
+        }
     }
 
     useEffect(() => {
         // setCity(cityMock)
-        if (!city.lenght){
-            return
-        }
+        // if (!city.lenght) {
+        //     return
+        // }
         LoadCities()
     }, [city])
 
     return (
         <Layout>
             <h1>Home</h1>
-            { city.map((item: any) => {
+            {city.map((item: any) => {
                 <p key={item.id}>{item?.nome} / {item?.estado}</p>
             })}
 
-            
+
         </Layout>
     );
 }
--- src/pages/Search/Search.tsx
+++ src/pages/Search/Search.tsx
@@ -9,20 +9,27 @@ export default function Search() {
     const [noResult, setNoResult] = useState<boolean>(false)
 
     const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-        if(event.target.value === ''){
+        if (event.target.value === '') {
             setHasSearch(false)
         }
 
         setCityName(event.target.value)
     }
 
-    const handleClick = () => {
-        setHasSearch(true)
-        if (cityName !== "Santo André"){
-            setNoResult(true)
-        }else{
-            setNoResult(false)
+    const LoadCities = async () => {
+        try {
+            const response = await fetch(`https://brasilapi.com.br/api/cptec/v1/cidade/${cityName}`)
+            const data = await response.json()
+            console.log(data);
+
+            setCityList(data)
+        } catch (error) {
+            console.log(error);
         }
+    }
+
+    const handleClick = () => {
+        LoadCities()
     }
 
     const handleClear = () => {
@@ -36,31 +43,31 @@ export default function Search() {
             <h1>Busca</h1>
             <form>
                 <label htmlFor="search">Buscar cidade</label>
-                <input 
-                    type="text" 
-                    name="search" 
-                    id="search" 
+                <input
+                    type="text"
+                    name="search"
+                    id="search"
                     onChange={handleChange}
                     value={cityName}
                 />
                 <button onClick={handleClick} type="button">Buscar</button>
             </form>
 
-            {hasSearch 
-                ? ( <div>
+            {hasSearch
+                ? (<div>
                     <p>Busca pela cidade: {cityName}</p>
                     <button onClick={handleClear}>Limpar busca</button>
-                    {noResult ? (<p>Nenhuma cidade encontrada!</p>) 
-                    : 
-                    <ul>
-                        {
-                            cityList.map((city) => (
-                                <li key={city.id}>
-                                    {city.nome} - {city.estado}
-                                </li>
-                            ))
-                        }
-                    </ul> }
+                    {noResult ? (<p>Nenhuma cidade encontrada!</p>)
+                        :
+                        <ul>
+                            {
+                                cityList.map((city) => (
+                                    <li key={city.id}>
+                                        {city.nome} - {city.estado}
+                                    </li>
+                                ))
+                            }
+                        </ul>}
                 </div>
                 ) : null
             }
